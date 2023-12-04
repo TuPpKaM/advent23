d5538d0dd15219c33f5935ce9633bc9bf8938a78
DESKTOP-OSFSGV9\Jeppe <mosaren@hotmail.com>
2023-12-04 17:09:26 +0100

    day4 cleaned

--- day1-9/d4.py
+++ day1-9/d4.py
@@ -1,4 +1,3 @@
-from util import caprint, crprint, make_empty_grid, colored_grid_display
 import re
 
 def one():
@@ -11,17 +10,16 @@ def one():
             game_num = int(game_num[0])
 
             win_, you_ = line.split('|')
-            win = win_.split(' ') #should have been .split()
-            you = you_.split(' ') #should have been .split()
+            win = win_.split()
+            you = you_.split()
 
             score = 0
             for num in you:
-                if num != '': #split() solves this
-                    if num in win:
-                        if score < 1:
-                            score = 1
-                        else:
-                            score += score
+                if num in win:
+                    if score < 1:
+                        score = 1
+                    else:
+                        score += score
 
             sums += score
 
@@ -29,7 +27,6 @@ def one():
 
 def two():
     sums = 0
-    scores = {}
     extra_wins = {}
     for _ in range(1,200):
         extra_wins[_] = 0
@@ -50,13 +47,10 @@ def two():
                 if num in win:
                     score += 1
 
-            scores[game_num] = score
             for i in range (game_num+1, game_num+score+1, 1):
-                extra_wins[i] += 1
-                if extra_wins[game_num] > 0:
-                    extra_wins[i] += extra_wins[game_num]
+                extra_wins[i] += 1 + extra_wins[game_num]
 
-    for num , amount in extra_wins.items():
+    for _ , amount in extra_wins.items():
         sums += amount
 
     sums += 199
